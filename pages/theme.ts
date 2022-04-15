29f0745e03d4f810f25950f840300a5bdb9e3a93
권은서 <kwonenseo@gmail.com>
2022-04-15 17:16:06 +0900

    Set lightTheme's default style

---
+++ pages/theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#F6F7F9",
+  mainColor: "#2F3396",
+  tagPurpleColor: "#E0C8DD",
+  tagBlueColor: "#D1E9F3",
+  tagGreenColor: "#BEE8BA",
+  tagYelloColor: "#F8D6B7",
+  textColor: "#000", 
+};
+
+// 추후 다크모드 추가 예정
+// export const darkTheme: DefaultTheme = {
+//   bgColor: "",
+//   mainColor: "",
+//   textColor: "",
+//   accentColor: "",
+// };
