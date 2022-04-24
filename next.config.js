e0315d86e2fbd3485963e5da9deafd35228b8f48
himchan94 <himchan9494@gmail.com>
2022-04-24 19:03:20 +0900

    fix: Fixed svgr conflict

--- next.config.js
+++ next.config.js
@@ -3,14 +3,3 @@ const nextConfig = {
   reactStrictMode: true,
 };
 module.exports = nextConfig;
-
-module.exports = {
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
-};
