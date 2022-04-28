0b98daace53c8ac11b2009ba13ad369948a03ec8
권은서 <kwonenseo@gmail.com>
2022-04-28 15:40:15 +0900

    .babelrc issue 해결

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ module.exports = {
   webpack(config, options) {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
       use: ["@svgr/webpack"],
     });
 
