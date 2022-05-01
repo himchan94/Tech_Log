d12fece57d134bd64af17004c81b3091ebadc7cc
himchan94 <himchan9494@gmail.com>
2022-05-01 12:13:30 +0900

    feat: Applied params

--- components/tag/Tag.tsx
+++ components/tag/Tag.tsx
@@ -12,6 +12,11 @@ const Tag: React.FC<TagProps> = ({ tagName }) => {
 export default Tag;
 
 const TagWrapper = styled.div`
+  display: inline-block;
   height: 17px;
   border-radius: 105px;
+  background-color: #e0c8dd;
+  padding: 2px 10px;
+  font-size: 10px;
+  font-weight: 400;
 `;
